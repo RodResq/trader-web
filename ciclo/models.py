546838ce797c1c699aafdcefd1b3ecb9eba1ef5d
RodResq <rodrigoresque@yahoo.com.br>
2025-04-23 20:34:08 -0300

    Refatora estrutura

--- ciclo/models.py
+++ ciclo/models.py
@@ -20,28 +20,3 @@ class Ciclo(models.Model):
         verbose_name = 'Ciclo'
         verbose_name_plural = 'Ciclo'
         ordering = ['-data_inicial']
-
-
-class GerenciaCiclo(models.Model):
-    ciclo = models.ForeignKey(Ciclo, db_column="id_ciclo", on_delete=models.CASCADE, null=True, blank=True, related_name="gerencia_ciclo", verbose_name="gerencia")
-    qtd_total_entrada = models.IntegerField(blank=True, null=True, default=0)
-    valor_total_entrada = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2, 
-        validators=[MinValueValidator(0)], 
-        verbose_name="Valor Total de Entrada", 
-        default=0.0
-    )
-    valor_total_retorno = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2, 
-        validators=[MinValueValidator(0)], 
-        verbose_name="Valor Total de Retorno", 
-        default=0.0
-    )
-    
-    class Meta:
-        managed = True
-        db_table = 'gerencia_ciclo'
-        verbose_name = 'GerenciaCiclo'
-        verbose_name_plural = 'Gerencia Entrada'
--- lucro/models.py
+++ lucro/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from ciclo.models import Ciclo
 
 # Create your models here.
 
@@ -36,3 +37,30 @@ class Resultado(models.Model):
         verbose_name = "Resultado"
         verbose_name_plural = "LucResultadoros"
         ordering = ["-semana"]
+
+
+
+
+class GerenciaCiclo(models.Model):
+    ciclo = models.ForeignKey(Ciclo, db_column="id_ciclo", on_delete=models.CASCADE, null=True, blank=True, related_name="gerencia_ciclo", verbose_name="gerencia")
+    qtd_total_entrada = models.IntegerField(blank=True, null=True, default=0)
+    valor_total_entrada = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)], 
+        verbose_name="Valor Total de Entrada", 
+        default=0.0
+    )
+    valor_total_retorno = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)], 
+        verbose_name="Valor Total de Retorno", 
+        default=0.0
+    )
+    
+    class Meta:
+        managed = True
+        db_table = 'gerencia_ciclo'
+        verbose_name = 'GerenciaCiclo'
+        verbose_name_plural = 'Gerencia Entrada'
