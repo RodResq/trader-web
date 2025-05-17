9a3b18640a5d30ed407eec22e10bce6fd43b97ab
RodResq <rodrigoresque@yahoo.com.br>
2025-05-16 22:37:29 -0300

    updt

--- templatetags/custom_filters.py
+++ templatetags/custom_filters.py
@@ -1,10 +1,7 @@
 from django import template
-from django.utils import timezone
 
 register = template.Library()
-
-@register.filter(name='to_utc')
-def to_utc(value):
-    if value is None:
-        return ""
-    return timezone.localtime(value, timezone=timezone.utc)
+        
+@register.filter
+def subtract(value, arg):
+    return value - arg
