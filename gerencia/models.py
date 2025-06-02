08401a4cecdf7c4883dceeb66ae4f09c703681cb
RodResq <rodrigoresque@yahoo.com.br>
2025-06-02 18:02:49 -0300

    feat: cria model EvolucaoSaldoAtual

--- gerencia/models.py
+++ gerencia/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from ciclo.models import Ciclo
+from datetime import date
 
 # Create your models here.
 class GerenciaCiclo(models.Model):
@@ -26,3 +27,35 @@ class GerenciaCiclo(models.Model):
         db_table = 'gerencia_ciclo'
         verbose_name = 'GerenciaCiclo'
         verbose_name_plural = 'Gerencia Entrada'
+        
+        
+class EvolucaoSaldoAtual(models.Model):
+    id_ciclo = models.ForeignKey(Ciclo, db_column="id_ciclo", on_delete=models.CASCADE, null=True, blank=True, related_name="evolucao_saldo_atual", verbose_name="ciclo")
+    saldo = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)], 
+        verbose_name="Valor do Saldo Atual", 
+        default=0.0
+    )
+    disponivel_entrada = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)], 
+        verbose_name="Valor Disponivel Entrada", 
+        default=0.0
+    )
+    data = models.DateField(verbose_name="Data da Alterção do sado inicial", default=date.today)
+    
+    class Meta:
+        managed = True
+        db_table = 'evolucao_saldo_atual'
+        verbose_name = 'Evolução Saldo Atual'
+        verbose_name_plural = 'Evoluçao dos Saldos Atuais'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['id_ciclo', 'data'],
+                name='unique_ciclo_data'
+            )
+        ]
+        
