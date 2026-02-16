a146a1073b330030c61cddea4e58b7157e9cd7b7
RodResq <rodrigoresque@yahoo.com.br>
2026-02-15 22:50:34 -0300

    feat: create team sports

--- prediction/views.py
+++ prediction/views.py
@@ -1,5 +1,10 @@
 from rest_framework import generics, permissions, status
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.db import transaction
+
+from team_sports.models import TeamSports, TeamSofascoreVsTeamSports
+from team.models import TeamSofascore
 
 import os
 import logging
@@ -15,7 +20,7 @@ class PredictionView(generics.GenericAPIView):
             id_team_home = request.query_params.get('idHome')
             name_home = request.query_params.get('nameHome')
             id_team_away = request.query_params.get('idAWay')
-            name_away = request.query_params.get('nameAWay')
+            name_away = request.query_params.get('nameAway')
             country = request.query_params.get('country')
             event_date = request.query_params.get('date')
             
@@ -23,7 +28,7 @@ class PredictionView(generics.GenericAPIView):
                 logger.info('Dados dos times e eventos nao encontrados')
             
             url = os.getenv('URL_BASE_API_SPORTS') + '/teams'  
-            requests.get(
+            response = requests.get(
                 url,
                 params ={
                     'name': name_home,
@@ -31,13 +36,47 @@ class PredictionView(generics.GenericAPIView):
                 },
                 headers={
                     'x-apisports-key': os.getenv('API_SPORTS_KEY')
-                },
-                timout=5
+                }
             )
+            
+            if not response:
+                return JsonResponse({
+                    'success': False,
+                    'erro': 'Team nao encontrado na api sports'
+                }, status=404)
+                
+            response_json = response.json()
+            team_sf = get_object_or_404(TeamSofascore, pk=id_team_home)
+            
+            with transaction.atomic():  
+                team_sports = self._create_team_sports(response_json);
+                self._create_intermediary_table(team_sf,  team_sports)
+            
+            return JsonResponse({
+                'sucess': True,
+                'message': 'Team Sports Criado'
+            })
         except requests.exceptions.RequestException as e:
             return JsonResponse({
                 'success': False,
                 'erro': str(e)
             }, status=500)
             
-            
+    def _create_team_sports(self, response):
+        response_obj = response.get('response', [])
+        response_team = response_obj[0].get('team', {})
+        team_sports = TeamSports.objects.create(
+            id= response_team.get('id'),
+            name = response_team.get('name'),
+            code = response_team.get('code'),
+            country = response_team.get('country'),
+            logo = response_team.get('logo') 
+        )
+        
+        return team_sports
+    
+    def _create_intermediary_table(self, team_sf, team_sports):
+        TeamSofascoreVsTeamSports.objects.create(
+            sofascore_team = team_sf,
+            sports_team = team_sports
+        )
--- setup/static/js/modules/entrada/predictions.js
+++ setup/static/js/modules/entrada/predictions.js
@@ -1,3 +1,5 @@
+import { showNotification } from "../notifications.js";
+import { apiClient } from "../shared/apiClient.js";
 
 export function setupPredictions() {
     const btnPredictions = document.querySelectorAll('.predictions');
@@ -19,16 +21,26 @@ export function setupPredictions() {
 
             console.log('Click em Predictions com event ID: ', idEvent);
             console.log('Click em Predictions com country: ', String(country).toLowerCase());
-            console.log('Click em Predictions com idHome e nameHome na data: ', idHome, String(nameHome).toLowerCase(), eventDate);
-            console.log('Click em Predictions com idAway e nameAway na data: ', idAWay, String(nameAWay).toLowerCase().replace("_", " "), eventDate);
+            console.log('Click em Predictions com idHome e nameHome na data: ', _formatTeamName(nameHome), eventDate);
+            console.log('Click em Predictions com idAway e nameAway na data: ', idAWay, _formatTeamName(nameAWay), eventDate);
 
+            const nameHomeFormated = _formatTeamName(nameHome);
+            const nameAwayFormated = _formatTeamName(nameAWay);
+
+            const url = `/predictions?idHome=${idHome}&nameHome=${nameHomeFormated}&idAway=${idAWay}&nameAway=${nameAwayFormated}&country=${country}&date=${eventDate}`;
             try {
+               const data = await apiClient.get(url);
+               console.log('Retorno Api Predictions: ', data);
                 
             } catch (error) {
-
+                showNotification(error.message, 'danger');
             }
             
         });
     });
 
+}
+
+function _formatTeamName(teamName) {
+    return String(teamName).toLowerCase().replace("_", " ");
 }
