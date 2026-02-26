17296151698ca542e0bd41033298f7e213484a4e
RodResq <rodrigoresque@yahoo.com.br>
2026-02-26 19:18:02 -0300

    Refactor: Use `Entrada.objects.get_or_create` with a `defaults` dictionary for creating new entries.

--- analytics/helpers.py
+++ analytics/helpers.py
@@ -7,28 +7,31 @@ def dump_mercados_para_entrada() -> bool:
     mercados = VwMercadoSuperFavoritoHome.objects.all()
     
     id_existents = Entrada.objects.values_list('id_event', flat=True)
+    
     contador_inseridos = 0
     contador_ignorados = 0
     
     with transaction.atomic():
         for mercado in mercados:
             if mercado.id_event not in id_existents:
-                Entrada.objects.create(
-                    id_event=mercado.id_event,
-                    id_home=mercado.id_team_home,
-                    icon_home_data_url=mercado.icon_home_data_url,
-                    name_home=mercado.name_home,
-                    id_away=mercado.id_team_away,
-                    icon_away_data_url=mercado.icon_away_data_url,
-                    name_away=mercado.name_away,
-                    mercado=mercado.mercado_name,
-                    odd=mercado.odd,
-                    home_actual=mercado.home_actual,
-                    home_vote=0,
-                    away_actual=mercado.away_actual,
-                    away_vote=0,
-                    data_jogo=mercado.data_jogo,
-                    country=mercado.country_name
+                Entrada.objects.get_or_create(
+                    id_event=mercado.id_event, 
+                    defaults={
+                        'id_home': mercado.id_team_home,
+                        'icon_home_data_url': mercado.icon_home_data_url,
+                        'name_home': mercado.name_home,
+                        'id_away': mercado.id_team_away,
+                        'icon_away_data_url': mercado.icon_away_data_url,
+                        'name_away': mercado.name_away,
+                        'mercado': mercado.mercado_name,
+                        'odd': mercado.odd,
+                        'home_actual': mercado.home_actual,
+                        'home_vote': 0,
+                        'away_actual': mercado.away_actual,
+                        'away_vote': 0,
+                        'data_jogo': mercado.data_jogo,
+                        'country': mercado.country_name,
+                    }
                 )
                 contador_inseridos += 1
             else:
@@ -36,6 +39,6 @@ def dump_mercados_para_entrada() -> bool:
     
     if contador_inseridos > 0:
         return True
-    return False;
+    return False
             
     
